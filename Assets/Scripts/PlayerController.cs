338ca90ebd059fa1327eb27c71a2be9124dd881a
Ronauli S <deasnataly@gmail.com>
2015-11-29 20:24:17 +0700

    Finale

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextShot = 0f;
+       
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextShot)
+        {
+            nextShot = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audio.Play();
+        }
+
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector3(moveHorizontal, 0f, moveVertical) * speed;
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+                    0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
+                
+        rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x * -tilt);
+    }
+}
